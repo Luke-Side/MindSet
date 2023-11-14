06dd00b01639a1a0fd29d20d03bb4b6f44924473
Pedro Marques <70141940+Pedroco11@users.noreply.github.com>
2023-11-14 10:20:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var numeroSecreto = parseInt(Math.random() * 1000 + 1);
 
 //enquanto o chute for diferente do número secreto
 while (chute != numeroSecreto) {
-  var chute = prompt ('Digite um número entre 1 e 00');
+  var chute = prompt ('Digite um número entre 1 e 1000');
   //se o chute for igual ao número secreto
   if (chute == numeroSecreto) {
     alert ('Nice, boa meu mano!👍')
