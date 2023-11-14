99c209d9009058104f796fd9fe838c6b0028fe69
Pedro Marques <70141940+Pedroco11@users.noreply.github.com>
2023-11-14 09:53:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var numeroSecreto = parseInt(Math.random() * 1000 + 1);
+
+
+//enquanto o chute for diferente do número secreto
+while (chute != numeroSecreto) {
+  var chute = prompt ('Digite um número entre 1 e 00');
+  //se o chute for igual ao número secreto
+  if (chute == numeroSecreto) {
+    alert ('Nice, boa meu mano!👍')
+  } else if (chute > numeroSecreto) {
+    alert('Errou, chuta baixo!')
+  } else if (chute < numeroSecreto) {
+    alert('Errou, ta pouco!')
+  }
+}
